23212a08184f1a2fcb419b31618e5690dfc6aa5c
paulchu756 <lordbabypanda@gmail.com>
2016-11-09 22:28:29 +0000

    api.php

--- public/project2/api.php
+++ public/project2/api.php
@@ -10,14 +10,24 @@ $connection->select_db($database);
 
 if($_SERVER["REQUEST_METHOD"] === "POST")
 {
-	var_dump($_POST);
+	//var_dump($_POST);
 	$id = intval($_POST["personID"]);
 	getPerson($id);
 }
 elseif($_SERVER["REQUEST_METHOD"] === "GET")
 {
-	var_dump($_GET);
-	$id = intval($_GET["personID"]);
+	//var_dump($_GET);
+	if(!empty($_GET["personID"]))
+	{
+		// select one person
+		$id= intval($_GET["personID"]);
+		getPerson($id);
+	}
+	else
+	{
+		// select everyone
+		getPerson();
+	}
 }
 else
 {
@@ -25,46 +35,56 @@ else
 	die();
 }
 
-function getPerson()
+function getPerson($id = 0)
 {
-	$resultSql =	"SELECT p.firstname, s.statename, p.food
+	$resultSql = "SELECT * FROM People";
+	if($id != 0)
+	{
+		$resultSql .=	"SELECT p.firstname, s.statename, p.food
 					FROM Visits v
 					INNER JOIN People p ON v.p_id = p.id
 					INNER JOIN States s ON v.s_id = s.id
 					WHERE v.p_id =" . $id;
 
-	$query = mysqli_query ($connection, $resultSql) or die(mysqli_error($connection));
-	$row2 = mysqli_fetch_array($query);
-		
-	$firstName = $row2["firstname"];
-	$stateName = $row2["statename"];
-	$foodName = $row2["food"];
+		$query = mysqli_query ($connection, $resultSql) or die(mysqli_error($connection));
+		$row2 = mysqli_fetch_array($query);
+			
+		$firstName = $row2["firstname"];
+		$stateName = $row2["statename"];
+		$foodName = $row2["food"];
 
-	// Display Json format here
-
-		if(!empty($firstName) && !empty($stateName) && !empty($foodName))
-		{
-			echo "<br><br><center> The human you select is : " . $firstName . "</center>";
-
-			echo "<br><center> The state they're visited : " . $stateName . "</center>";
-
-			while($row3 = mysqli_fetch_array($query))
+			if(!empty($firstName) && !empty($stateName) && !empty($foodName))
 			{
-				$stateName = $row3["statename"];
-				echo "<br><center> The state they're visited : " . $stateName . "</center>";				
+				echo "<br><br><center> The human you select is : " . $firstName . "</center>";
+
+				echo "<br><center> The state they're visited : " . $stateName . "</center>";
+
+				while($row3 = mysqli_fetch_array($query))
+				{
+					$stateName = $row3["statename"];
+					echo "<br><center> The state they're visited : " . $stateName . "</center>";				
+				}
+
+				echo "<br><center> Their favor food is : " . $foodName . "<br><br><br><br></center>";
 			}
 
-			echo "<br><center> Their favor food is : " . $foodName . "<br><br><br><br></center>";
-		}
+			else
+			{
+				echo "<br><center> You need to add a visit </center>";
+			}
+	}
+	
+	$response = array();
+	$query = mysqli_query ($connection, $resultSql) or die(mysqli_error($connection));
 
-		else
-		{
-			echo "<br><center> You need to add a visit </center>";
-		}
+
+	
 }
 
+/*
 function insertPerson()
 {
 
 }
+*/
 ?>
--- public/project2/drive.js
+++ public/project2/drive.js
@@ -17,28 +17,3 @@ $(document).ready(function(){
 		});
 	});
 });
-
-/*		
-Pure javascript, doesn't use Jquery at all
-function getInfo(str)
-{
-	if(str == "")
-	{
-		return document.getElementById("form").innerHTML = "";
-	}
-	else 
-	{
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function()
-		{
-			// readyState 4 means complete && 200 means complete
-			if(this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("form").innerHTML = this.responseText;
-			}
-		};
-		xmlhttp.open("GET","api.php?q="+str, true);
-		xmlhttp.send();
-	}
-}
-*/
