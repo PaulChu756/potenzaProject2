64ade9e01c8c3caee6bc43a500725a4d8123307f
paulchu756 <lordbabypanda@gmail.com>
2016-11-09 22:50:08 +0000

    added person/visit function

--- public/project2/api.php
+++ public/project2/api.php
@@ -8,15 +8,12 @@ if(!$connection){
 die("Could not connect: " . mysqli_connect_error());}
 $connection->select_db($database);
 
-if($_SERVER["REQUEST_METHOD"] === "POST")
+// testing
+var_dump($_GET);
+var_dump($_POST);
+
+if($_SERVER["REQUEST_METHOD"] === "GET")
 {
-	//var_dump($_POST);
-	$id = intval($_POST["personID"]);
-	getPerson($id);
-}
-elseif($_SERVER["REQUEST_METHOD"] === "GET")
-{
-	//var_dump($_GET);
 	if(!empty($_GET["personID"]))
 	{
 		// select one person
@@ -29,13 +26,19 @@ elseif($_SERVER["REQUEST_METHOD"] === "GET")
 		getPerson();
 	}
 }
+
+elseif($_SERVER["REQUEST_METHOD"] === "POST")
+{
+	insertPerson();
+}
+
 else
 {
 	echo "Request method is bad, you should feel bad";
 	die();
 }
 
-function getPerson($id = 0)
+function getPerson($id=0)
 {
 	$resultSql = "SELECT * FROM People";
 	if($id != 0)
@@ -76,15 +79,65 @@ function getPerson($id = 0)
 	
 	$response = array();
 	$query = mysqli_query ($connection, $resultSql) or die(mysqli_error($connection));
-
-
-	
+	while($row = mysqlit_fetch_array($query))
+	{
+		$response[] = $row;
+	}
+	header('Content-Type: application/json');
+	echo json_encode($response);
 }
 
-/*
 function insertPerson()
 {
+	// define variables to be all empty
+	$firstNameError = $lastNameError = $foodError = "";
+	$firstNameEnter = $lastNameEnter = $foodEnter = "";
 
+	$firstNameEnter = $_POST["firstName"];
+	$lastNameEnter = $_POST["lastName"];
+	$foodEnter = $_POST["food"];
+
+	if(!empty($firstNameEnter) && !empty($lastNameEnter) && !empty($foodEnter))
+	{
+	// Insert values into table
+	$sql = "INSERT INTO People (firstname, lastname, food) 
+	VALUES('$firstNameEnter', '$lastNameEnter', '$foodEnter')";
+		// Check if insert is good
+		if($connection->query($sql) === FALSE)
+		{
+		echo "Error: " . $sql . "<br>" . $connection->error;
+		}
+	}
+	else 
+	{
+	$firstNameError = "First Name is required";
+	$lastNameError = "Last Name is required";
+	$foodError = "Food is required NOW!";
+	}
 }
-*/
+
+function insertVisit()
+{
+	$visitError = $visitEnter = "";
+
+	$personEnter = $_POST["humanName"];
+	$stateEnter = $_POST["stateName"];
+	$visitEnter = $_POST["visit"];
+		
+	if(!empty($visitEnter))
+	{
+	$visitSql = "INSERT INTO Visits(p_id, s_id, date_visited)
+	VALUES('$personEnter', '$stateEnter', '$visitEnter')";
+
+		// Check if insert is good
+		if($connection->query($visitSql) === FALSE)
+		{
+			echo "Error: " . $visitSql . "<br>" . $connection->error;
+		}
+	}
+	else
+	{
+		$visitError = "Data visit is required";
+	}
+}
 ?>
