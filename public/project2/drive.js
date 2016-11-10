46554586d9548682ba2a4a925a04b34064b40dd8
paulchu756 <lordbabypanda@gmail.com>
2016-11-10 19:35:12 +0000

    trying to populate dropdown

--- public/project2/drive.js
+++ public/project2/drive.js
@@ -1,5 +1,27 @@
 <script src="jquery.js"></script>
 
+function populatePeople()
+{
+	$('#Name').change(function(){
+	var getInfo = $(this).val();
+		$.ajax({
+			type:"GET",
+			url:"api.php",
+			data:{personID:getInfo},
+			dataType:"json",
+			success: function(json)
+			{
+				$('#Name').empty();
+				$('#Name').append("<option='0'>Select a Human</option>");
+				$.each(json, function(value,key)
+				{
+					$('$Name').append("<option></option>")
+				}
+			}
+		})
+	})
+}
+
 $(document).ready(function(){
 	$('#Name').change(function(){
 		var getInfo = $(this).val();
