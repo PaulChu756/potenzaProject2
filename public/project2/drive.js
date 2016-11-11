890fe6a875af8e2b73e50b9d7b0ec45051fdd0ff
PaulChu <PaulChu>
2016-11-11 16:45:54 -0600

    testing

--- public/project2/drive.js
+++ public/project2/drive.js
@@ -1,32 +1,11 @@
 console.log("hello");
-
-/*$(document).ready(function(){
-	console.log("cookies");
-	$('#Name').change(function(){
-	populatePeople();
-	});
-});
-
-function populatePeople()
-{
-	var getInfo = $(this).val();
-		$.ajax({
-			type:"GET",
-			url:"api.php",
-			data:{personID:getInfo},
-			dataType:"json",
-			success: function(json)
-			{
-				var name = $("#Name");
-				$name.empty();
-				$name.append($("<option></option>")
-					.attr("value","").text("Select a Human"));
-				$.each(json, function(value,key)
-				{
-					$name.append($("<option></option>")
-						.attr("value", value).text(key));
-				});
-			}
-	});
-}
-*/
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("butt");
+console.log("cookies");
+console.log("cookies");
+console.log("cookies");
+console.log("cookies");
+console.log("cookies");
