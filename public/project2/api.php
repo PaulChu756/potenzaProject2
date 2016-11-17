907ba7d978f3f2768054905470f461cfc52155ee
Chui <lordbabypanda@gmail.com>
2016-11-17 00:22:52 +0000

    delete comment

--- public/project2/api.php
+++ public/project2/api.php
@@ -69,36 +69,6 @@ function getPerson($id=0)
 	if($id != 0)
 	{
 		$resultSql.=" WHERE id=". $id ." LIMIT 1";
-		/*
-		$resultSql = "SELECT p.firstname, s.statename, p.food
-					FROM Visits v
-					INNER JOIN People p ON v.p_id = p.id
-					INNER JOIN States s ON v.s_id = s.id
-					WHERE v.p_id =" . $id;
-
-		$query = mysqli_query ($connection, $resultSql) or die(mysqli_error($connection));
-		$row2 = mysqli_fetch_array($query);
-			
-		$firstName = $row2["firstname"];
-		$stateName = $row2["statename"];
-		$foodName = $row2["food"];
-		
-			if(!empty($firstName) && !empty($stateName) && !empty($foodName))
-			{
-				echo "<br><br><center> The human you select is : " . $firstName . "</center>";
-				echo "<br><center> The state they're visited : " . $stateName . "</center>";
-				while($row3 = mysqli_fetch_array($query))
-				{
-					$stateName = $row3["statename"];
-					echo "<br><center> The state they're visited : " . $stateName . "</center>";				
-				}
-				echo "<br><center> Their favor food is : " . $foodName . "<br><br><br><br></center>";
-			}
-			else
-			{
-				echo "<br><center> You need to add a visit </center>";
-			}
-			*/
 	}
 	
 	$response = array();
